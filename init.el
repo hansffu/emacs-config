6e5a837784977b78b752c9062b9f01a4c6575bf6
Hans Fredrik Furholt <hansff@gmail.com>
2018-03-13 00:49:04 +0100

    Blue statusbar and magit

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(package-initialize)
+
 (setq inhibit-splash-screen t)
 (linum-mode 1)
 (tool-bar-mode 0)
@@ -8,3 +10,16 @@
 ;(set-foreground-color "#CFD8DC")
 
 (load-theme 'adapta-noko-maia t)
+
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+;;Package stuff
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(custom-set-variables
+ '(package-selected-packages (quote (magit))))
+
