8ec138d1fc9a067aa81cb32735b4737ca617dcea
Hans Fredrik Furholt <hansff@gmail.com>
2018-03-13 00:26:31 +0100

    start color theme

--- init.el
+++ init.el
@@ -3,4 +3,8 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
-(load-theme 'misterioso)
+
+;(set-background-color "#222D32")
+;(set-foreground-color "#CFD8DC")
+
+(load-theme 'adapta-noko-maia t)
